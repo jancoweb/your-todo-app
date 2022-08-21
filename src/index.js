4cd8f4fe2fb904da0e138b126c0c75a3abad1cc1
Joao Coelho <jancoweb@outlook.com>
2022-08-21 03:41:24 -0300

    hotfix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors')
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(cors())
 app.use(routes);
 
 app.listen(process.env.PORT || 3001)
