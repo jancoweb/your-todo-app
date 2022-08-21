6c7152f23b21e8e14499cd05bd24adb136ae908b
Joao Coelho <jancoweb@outlook.com>
2022-08-21 03:23:06 -0300

    SQL database schema

---
+++ src/schema.sql
@@ -0,0 +1,15 @@
+create database todoapp
+
+create table users(
+  id serial primaty key,
+  name text not null,
+  email text not null,
+  password text not null
+)
+
+create table tasks (
+	id serial primary key,
+  user_id int,
+  title text not null,
+  foreign key (user_id) references users(id)
+)
