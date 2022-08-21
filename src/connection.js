e781ac1b9442ef1177ce2b7ebebc251649ac675f
Joao Coelho <jancoweb@outlook.com>
2022-08-21 03:54:05 -0300

    removing port var. from connection

--- src/connection.js
+++ src/connection.js
@@ -4,8 +4,7 @@ const knex = require('knex')({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+    database: process.env.DB_DATABASE
   },
   ssl: {
     rejectUnauthorized: false
